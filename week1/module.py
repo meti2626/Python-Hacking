6e18d5a684a0c5f8b1c06f3dfa7d26611700767c
meti2626 <metseboka@gmail.com>
2025-06-17 10:22:55 +0300

    new commit

---
+++ week1/module.py
@@ -0,0 +1,19 @@
+import mymodule
+import os
+
+import sys
+
+# print(mymodule.generate_full_name('john' , 'doe'))
+
+# Creating a directory 
+# os.mkdir('travi')
+
+# print("Directory 'travi' created successfully.")
+# print(os.getcwd())
+# os.rmdir('travi')
+# print("Directory 'travi' removed successfully.")
+
+# print('Welcome {}. Enjoy  {} .  say {} challenge!'.format(sys.argv[1], sys.argv[2] , sys.argv[3]))
+
+print(sys.maxsize)
+print(sys.path)
---
+++ week1/mymodule.py
@@ -0,0 +1,7 @@
+# mymodule.py file
+def generate_full_name(firstname, lastname):
+    return firstname + ' ' + lastname
+
+
+
+
