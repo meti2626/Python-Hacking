5f84c6a26cb655dcb7904ee90d07d92a4da24ec1
meti2626 <metseboka@gmail.com>
2025-06-28 21:56:09 +0300

    Update xss.py

--- week4/xss.py
+++ week4/xss.py
@@ -1,17 +1,8 @@
-#  XSS  , explain it from hacking perspective and python learning perspective , but before we get in to technical perspective explain this like am 5 wt real world example
-
-import requests
-
-url = "https://xss-game.appspot.com/search?q="
+/url = "https://xss-game.appspot.com/search?q="
 
 
 payload = "<script>alert('XSS Attack!');</script>"
 
 full_url = url + payload
 
-r = requests.get(full_url)
-
-
-if payload in r.text:
-    print("XSS vulnerability found!")
-else:    print("No XSS vulnerability found.")
+r = requests.get(full_url
